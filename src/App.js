b8f870f5a8e2c58450684323660227b577a96ac4
OlviXhaferaj <olvixhaferaj@gmail.com>
2022-11-22 21:10:35 +0100

    Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect,useState } from 'react';
+import Axios from 'axios';
 
 function App() {
+  const [state,setState] = useState([]);
+
+  useEffect(()=>{
+    Axios.get('https://pokeapi.co/api/v2/pokemon?limit=807')
+
+    .then(response => {
+      setState((response.data.results))
+      console.log(response.data.results)
+    }).catch((err) => console.log(err))
+  }, [])
+  console.log(state)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        state.map((object, index) => {
+          return(
+            <div key={index}>
+              <p>{index+1}.{object.name}</p>
+            </div>
+          )
+        })
+      }
     </div>
   );
 }
